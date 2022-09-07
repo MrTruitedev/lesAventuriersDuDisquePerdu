c02fc3ad478c55e84c966d060217ed89c3ba862a
MrTruitedev <96184611+MrTruitedev@users.noreply.github.com>
2022-09-07 17:05:45 +0200

    Ajout java/testJava
    Initiation JAVA premiere partie

---
+++ java/testJava/src/thomas/Add.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package thomas;
+
+/**
+ *
+ * @author thomas
+ */
+public class Add {
+    
+    int a,b; 
+    public Add(int a, int b){
+        this.a = a;
+        this.b = b;
+    }
+    public  int res(){
+        return (a+b);
+    }
+}
