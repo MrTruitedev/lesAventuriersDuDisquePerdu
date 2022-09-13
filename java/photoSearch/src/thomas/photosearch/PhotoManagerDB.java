e521377b557557467a946b92f91fffd70a780866
MrTruitedev <tomtutti@hotmail.fr>
2022-09-13 12:18:54 +0200

    Update PhotoManagerDB.java
    Ajout de commentaires

--- java/photoSearch/src/thomas/photosearch/PhotoManagerDB.java
+++ java/photoSearch/src/thomas/photosearch/PhotoManagerDB.java
@@ -72,43 +72,57 @@ public class PhotoManagerDB {
     }
 
     void insert(DirectoryInfo di, DirectoryInfo kl) throws SQLException {
-
+        // Initialisation des variables idCatalog et idKeyword utilisées pour remplir la table link_keywords
         int idCatalog = -1;
         int idKeyword = -1;
 
         try {
-
+            //Requete d'insertion dans la table catalog
+            //Recupere les infos contenues dans di[0](->key) et di[1](->values)
             String sqlCatalog = "INSERT INTO catalog(" + di.getAllKeyValueForCatalog()[0] + ") VALUES (" + di.getAllKeyValueForCatalog()[1] + ")";
-
+            //Préparation de la requete et ajout d'un statement afin de récuperer l'id de la derniere ligne inserée
             PreparedStatement ps1 = con.prepareStatement(sqlCatalog, Statement.RETURN_GENERATED_KEYS);
+            //Execution de la requete
             ps1.executeUpdate();
+            //Recuperation de l'id de la derniere ligne inserée dans rsCatalog
             ResultSet rsCatalog = ps1.getGeneratedKeys();
             if (rsCatalog.next()) {
                 idCatalog = rsCatalog.getInt(1);
             }
             System.out.println("LAST INSERTED ID CATALOG >>>>>" + idCatalog);
-
+            //Initialisation de la variable splitKeywords
+            //Recupere plusieurs chaines de caracteres puis la découpe suivant la regex " " afin de retourner un tableau contenant une chaine de caractere par ligne 
             String[] splitKeywords = kl.getAllKeywords().split(" ");
-
+            //Boucle pour inserer les keywords dans la table keyword, un keyword par id
             for (int i = 0; i < splitKeywords.length; i++) {
+                //Si splitKeywords ne contient pas de keyword on ne l'inscrit pas en bdd
                 if (splitKeywords[i].length() != 0) {
+                    //Initialisation de la variable size
+                    //Elle permettra d'effectuer le test si le keyword est déja existant en bdd
                     int size;
+                    //Recherche de l'id du mot clé que l'on voudrait inserer
                     String sqlKeywordsSelect = "SELECT id FROM `keywords` WHERE keyword = '" + splitKeywords[i] + "'";
                     Statement ps2 = con.createStatement();
                     ResultSet rsSelect = ps2.executeQuery(sqlKeywordsSelect);
                     rsSelect.last();
                     size = rsSelect.getRow();
                     rsSelect.beforeFirst();
+                    //Si size = 0 cela indique que le mot clé que l'on essaie d'inserer n'existe pas en bdd
                     if (size == 0) {
+                        //Requete d'insertion dans la table keyword
                         String sqlKeywordsInsert = "INSERT INTO keywords(keyword) VALUES ('" + splitKeywords[i] + "')";
                         System.out.println("KEYWORD>>>>> " + splitKeywords[i]);
+                        //Préparation de la requete et ajout d'un statement afin de récuperer l'id de la derniere ligne inserée
                         PreparedStatement psKey = con.prepareStatement(sqlKeywordsInsert, Statement.RETURN_GENERATED_KEYS);
                         psKey.executeUpdate();
+                          //Recuperation de l'id de la derniere ligne inserée dans rsKeyword
                         ResultSet rsKeyword = psKey.getGeneratedKeys();
                         if (rsKeyword.next()) {
                             idKeyword = rsKeyword.getInt(1);
                         }
                     }
+                    //Requete d'insertion dans la table keyword
+                    //Elle recupere le dernier id inseré dans la table catalog et l'associe aux id des keyword correspondant
                     String sqlLinkInsert = "INSERT INTO link_keywords(id_catalog, id_key) VALUES ('" + idCatalog + "', (SELECT keywords.id FROM keywords WHERE keyword = '" + splitKeywords[i] + "'))";
                     PreparedStatement psLink = con.prepareStatement(sqlLinkInsert);
                     psLink.execute();
